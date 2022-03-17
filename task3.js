2e2d1c1901201ee3d95f2857bde1f4ae251c4552
Andry Pebrianto <andrypeb227@gmail.com>
2022-03-17 18:06:35 +0700

    Menambahkan validasi di task3 dengan MAX_SAFE_INTEGER

--- task3.js
+++ task3.js
@@ -1,13 +1,22 @@
 const divideAndSort = (angka) => {
-  if (typeof angka === "number") {
-    let angkaArr = angka
-      .toString()
-      .split(0)
-      .map((item) => item.split("").sort().join(""));
-    console.log(parseInt(angkaArr.join("")));
+  if(angka > Number.MAX_SAFE_INTEGER) {
+    if (typeof angka === "number") {
+      const hasil = angka
+        .toString()
+        .split(0)
+        .map((item) => item.split("").sort().join(""))
+        .join("");
+  
+      console.log(hasil);
+    } else {
+      console.log("Parameter pertama (angka) harus bertipe number.");
+    }
   } else {
-    console.log("Parameter pertama (angka) harus bertipe number.");
+    console.log("Parameter pertama (angka) tidak boleh lebih dari 9007199254740991");
   }
 };
 
-divideAndSort(23107560352023);
+divideAndSort(5956560159466056);
+// [595656]
+// [159466]
+// [56]
